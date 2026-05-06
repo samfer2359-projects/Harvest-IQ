a13b55bfc89edfaa7eb24df8219439fefb358ba9
samfer2359-projects <samfer2359@gmail.com>
2026-05-06 14:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -294,5 +294,6 @@ def logout():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
